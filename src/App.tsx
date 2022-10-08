cc26372ed119d1fdd4487308f5f5ecd1ef90c781
CyberNoone <106311679+Noonekissfm@users.noreply.github.com>
2022-10-08 15:21:08 +0300

    feat(AppCheckbox) refactoring
    reafactoring AppCheckbox and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AppCheckbox } from './components/AppCheckbox/AppCheckbox';
+import { size } from './types/sizes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className='buttonRow'>
+                <AppCheckbox checked={false} size={size.small} color="red" />
+                <AppCheckbox checked={true} size={size.medium} color="hotpink" />
+                <AppCheckbox checked={true} size={size.small} color="pink" />
+                <AppCheckbox checked={false} size={size.large} color="gold" />
+            </div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/components/AppCheckbox/AppCheckbox.tsx
+++ src/components/AppCheckbox/AppCheckbox.tsx
@@ -1,39 +1,30 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
+import { size } from '../../types/sizes';
 import './style.css';
 
-interface IProps {
+interface IProps<T> {
     color: string;
-    size: 'small' | 'medium';
+    size: T;
     checked: boolean;
 }
 
-export const AppCheckbox: FC<IProps> = ({ color = '#6c43bf', checked = false, size = 'medium' }) => {
+export const AppCheckbox: FC<IProps<size>> = ({ color = '#6c43bf', checked = false, size }) => {
     const [isChecked, setChecked] = useState(checked);
 
     const setCheckedHandler = () => {
         setChecked(!isChecked);
     };
 
-    useEffect(() => {
-        const property = document.documentElement.style;
-        switch (size) {
-            case 'medium':
-                property.setProperty('--togglerHandleSize', '32px');
-                return;
-            case 'small':
-                property.setProperty('--togglerHandleSize', '16px');
-        }
-    });
-
     return (
-        <div>
+        <div className={`inputWrapper inputWrapper--${size}`}>
             <input
+                className={`tumbler-input tumbler--${size}`}
                 type="checkbox"
                 checked={isChecked}
                 style={{ backgroundColor: `${!isChecked ? '#c2c2c2' : color}` }}
                 onChange={setCheckedHandler}
             />
-            <span>
+            <span className={`tumbler-check tumbler--${size}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill={color}>
                     <path d="M0 0h24v24H0z" fill="none" />
                     <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
