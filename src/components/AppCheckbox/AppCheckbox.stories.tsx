0db66cab4697605d5f5d85e216735f2b0d8a4bd7
CyberNoone <106311679+Noonekissfm@users.noreply.github.com>
2022-10-09 21:42:26 +0300

    feat(AppCheckbox) renaming stories and general css reset

--- src/components/AppCheckbox/AppCheckbox.stories.tsx
+++ src/components/AppCheckbox/AppCheckbox.stories.tsx
@@ -2,10 +2,10 @@ import { AppCheckbox } from './AppCheckbox';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 export default {
-    title: 'AppCheckbox',
+    title: 'Base Components',
     component: AppCheckbox,
 } as ComponentMeta<typeof AppCheckbox>;
 
 const Template: ComponentStory<typeof AppCheckbox> = args => <AppCheckbox {...args} />
 
-export const FirstStory = Template.bind({});
+export const Tumbler = Template.bind({});
